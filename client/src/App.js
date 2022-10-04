c3235d831eb999e119ff55b8bad3ce431448145c
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-03 23:00:51 -0400

    Project setup

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import './output.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p className="bg-indigo-600">Test</p>
+    </div>
+  );
+}
+
+export default App;
