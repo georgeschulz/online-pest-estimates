0183a9be026a8d1e2544df2b4cc8988f75e85b93
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-04 18:28:44 -0400

    Build front end router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import './output.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/scrollToTop/scrollToTop.js';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <ScrollToTop>
+        <Routes>
+          <Route path="/" element={<p>Home</p>} />
+          <Route path="/login" element={<p>Login</p>} />
+          <Route path="/signup/1" element={<p>Signup</p>} />
+          <Route path="/signup/2" element={<p>Business Congif</p>} />
+          <Route path="/widgets" element={<p>Widgets</p>} />
+          <Route path="*" element={<p>404 Not Found</p>} />
+        </Routes>
+      </ScrollToTop>
+    </Router>
   );
 }
 
