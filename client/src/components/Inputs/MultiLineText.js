77b52bf8476ab43292ea3babe13e532b33b39780
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-16 09:27:29 -0400

    Add multiline text component

---
+++ client/src/components/Inputs/MultiLineText.js
@@ -0,0 +1,33 @@
+function MultiLineText({name, label, type = 'text', state = "", setState, helper, showLabel = true, size = "medium", length = 100000}) {
+    let customStyles = "";
+
+    switch(size) {
+        case "short":
+            customStyles += 'py-2 px-8 text-xl h-24'
+            break;
+        default:
+            customStyles += 'py-2 px-8 text-xl h-36';
+            break;
+    }
+
+    return (
+        <div className="input-group mb-6">
+            {showLabel && (<label 
+                htmlFor={name}
+                className="text-xl font-roboto font-semibold ml-6">
+                {label}
+            </label>)}
+            <textarea
+                name={name} 
+                type={type} 
+                value={state}
+                placeholder={helper} 
+                onChange={(e) => setState(e.target.value)}
+                maxLength={length}
+                className={`border border-lightgray w-full rounded-lg text-lightmatte ${customStyles}`}
+            />
+        </div>
+    )
+}
+
+export default MultiLineText;
--- client/src/pages/details.js
+++ client/src/pages/details.js
@@ -2,6 +2,7 @@ import SingleLineText from "../components/Inputs/SingleLineText";
 import ApplicationMainLayout from "../components/layout/ApplicationMainLayout/ApplicationMainLayout";
 import { useDispatch, useSelector } from "react-redux";
 import { seelctBilling, selectBenefitOne, selectBenefitThree, selectBenefitTwo, selectFrequency, selectProgramDescription, selectProgramName, selectTargets, updateDraft } from "../redux/widgetSlice";
+import MultiLineText from "../components/Inputs/MultiLineText";
 
 function Details() {
     const name = useSelector(selectProgramName);
@@ -29,6 +30,46 @@ function Details() {
                             size="medium"
                             state={name}
                             setState={(e) => dispatch(updateDraft({name: e}))}
+                        />
+                        <MultiLineText
+                            name="program-description"
+                            label="Program Description"
+                            type="text"
+                            helper="Sell the benefits of your program here. Max 250 characters."
+                            state={description}
+                            length={250}
+                            setState={(e) => dispatch(updateDraft({programDescription: e}))}
+                        />
+
+                        <MultiLineText
+                            name="benefit-one"
+                            label="Benefit 1"
+                            type="text"
+                            helper="Write a clear benefit for your service tile."
+                            size="short"
+                            length={125}
+                            state={benefitOne}
+                            setState={(e) => dispatch(updateDraft({benefitOne: e}))}
+                        />
+                        <MultiLineText
+                            name="benefit-twp"
+                            label="Benefit 2"
+                            type="text"
+                            helper="Write a clear benefit for your service tile."
+                            size="short"
+                            length={125}
+                            state={benefitTwo}
+                            setState={(e) => dispatch(updateDraft({benefitTwo: e}))}
+                        />
+                        <MultiLineText
+                            name="benefit-three"
+                            label="Benefit 3"
+                            type="text"
+                            helper="Write a clear benefit for your service tile."
+                            size="short"
+                            length={125}
+                            state={benefitThree}
+                            setState={(e) => dispatch(updateDraft({benefitThree: e}))}
                         />
                     </div>
                     <div className="w-1/2">DEMO</div>
