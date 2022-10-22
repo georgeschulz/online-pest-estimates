041143b31686a95cecc3476784957cadd1a737a5
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-22 10:02:59 -0400

    Handler to add and remove options from multisel

--- client/src/components/Inputs/MultiSelectConfig.js
+++ client/src/components/Inputs/MultiSelectConfig.js
@@ -1,20 +1,10 @@
-import { useRef } from "react";
 import SingleLineText from "./SingleLineText";
+import x from '../../assets/x.png'
+import plus from '../../assets/plus.png'
 
 function MultiSelectConfig({ parameter, parameterIndex, parameters, config, updateConfig = () => {} }) {
-    const focusRef = useRef(null);
-
-    const updateValue = (e, i, type) => {
-        const { options } = parameter;
-        //update the options array and only specifically the one property we are targeting
-        const partOne = options.slice(0, i);
-        const partTwo = options.slice(i + 1, options.length);
-        const elementToUpdate = {
-            ...options[i],
-            [type]: e
-        }
-        const newOptions = [...partOne, elementToUpdate, ...partTwo];
-        
+    //this one is used in all the different handlers to take the update options and add them to the config object
+    const setOptionChanges = (newOptions) => {
         //create the new parameter array
         const parametersPart1 = parameters.slice(0, parameterIndex);
         const parametersPart2 = parameters.slice(parameterIndex + 1, parameters.length);
@@ -31,8 +21,38 @@ function MultiSelectConfig({ parameter, parameterIndex, parameters, config, upda
         }
 
         updateConfig(newConfig)
+   }
+
+    const updateValue = (e, i, type) => {
+        const { options } = parameter;
+        //update the options array and only specifically the one property we are targeting
+        const partOne = options.slice(0, i);
+        const partTwo = options.slice(i + 1, options.length);
+        const elementToUpdate = {
+            ...options[i],
+            [type]: e
+        }
+        const newOptions = [...partOne, elementToUpdate, ...partTwo];
+        setOptionChanges(newOptions)
     }
     
+    const removeOption = (i) => {
+        const { options } = parameter;
+        const partOne = options.slice(0, i);
+        const partTwo = options.slice(i + 1, options.length);
+        const newOptions = [...partOne, ...partTwo];
+        setOptionChanges(newOptions)
+    }
+
+    const addOption = () => {
+        const { options } = parameter;
+        const newOptions = [...options, {
+            option: '',
+            value: ''
+        }]
+        setOptionChanges(newOptions)
+    }
+
     return (
         <div className="border-stone-600 border p-5 mb-8">
             <p className="text-2xl ml-4 mb-5">{parameter.name} Configuration</p>
@@ -63,10 +83,21 @@ function MultiSelectConfig({ parameter, parameterIndex, parameters, config, upda
                                 setState={(e) => updateValue(e, i, 'value')}
                             />
                         </div>
-
+                        <div className="w-48 flex justify-center mt-4">
+                            <span className="flex scale" onClick={() => removeOption(i)}>
+                                <img className="w-5 h-5 mr-2" src={x} />
+                                <span>Remove Option</span>
+                            </span>   
+                        </div>
                     </div>
                 )
             })}
+            <div className="w-full">
+                <span className="flex text-2xl items-center text-green-800" onClick={() => addOption()}>
+                    <b className="text-4xl pointer scale mr-2">+</b> 
+                    <span className="pointer scale">Add Option</span>
+                </span> 
+            </div>
         </div>
     )
 }
