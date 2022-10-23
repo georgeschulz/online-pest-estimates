e6fe4ba7c62f6b474fa9f6a6d03fc02a4faccf26
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-23 18:59:30 -0400

    Fix null issue with active column

--- server/model/createUser.js
+++ server/model/createUser.js
@@ -2,7 +2,7 @@ const db = require('./db');
 
 module.exports = async (username, hashedPassword) => {
     try { 
-        const users = await db.query(`INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *`, [username, hashedPassword])
+        const users = await db.query(`INSERT INTO users (email, password, active) VALUES ($1, $2, false) RETURNING *`, [username, hashedPassword])
         return users.rows
     } catch (err) {
         throw err;
