f958eedcaa0058626660941773ef4336595dad7d
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-23 20:58:21 -0400

    Fix naming error in get widget

--- server/model/getWidget.js
+++ server/model/getWidget.js
@@ -8,13 +8,13 @@ module.exports = async (widgetId) => {
         const { rows: proposal } = await db.query(`SELECT 
             proposal_templates.proposal_template_id,
             proposal_templates.legal,
-            proposal_templates.covered_pests,
+            proposal_templates.coveredPests,
             highlighted_features.text,
             highlighted_features.is_included
         FROM proposal_templates LEFT JOIN highlighted_features ON highlighted_features.proposal_template_id = proposal_templates.proposal_template_id WHERE widget_id = $1`, 
         [widgetId]);
 
-        const { legal, covered_pests, proposal_template_id } = proposal[0];
+        const { legal, coveredPests, proposal_template_id } = proposal[0];
         
         const { rows: pricingStrategy } = await db.query(`SELECT * FROM pricing_strategies WHERE widget_id = $1`, [widgetId])
         const { rows: targetQuery }  = await db.query(`SELECT * FROM targets WHERE widget_id = $1`, [widgetId]);
@@ -24,7 +24,7 @@ module.exports = async (widgetId) => {
             widgetId, 
             widgetBasic[0], 
             details[0], 
-            { legal, covered_pests: covered_pests.split(','), proposal_template_id }, 
+            { legal, covered_pests: coveredPests.split(','), proposal_template_id }, 
             pricingStrategy[0], 
             benefitQuery.map(benefit => benefit.text), 
             targetQuery.map(target => target.name),
