d236f26c6b3c35bf3a6bcdf28a6ebe9444852a61
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-10-24 18:44:21 -0400

    Using window location change

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -27,8 +27,7 @@ function Signup() {
         (async () => {
             try {
                 if(process.env.NODE_ENV === 'production') {
-                    await fetch('https://onlinepestestimates.herokuapp.com/auth/login-google')
-                    //window.location.href = 'http://onlinepestestimates.herokuapp.com/auth/login-google'
+                    window.location.href = 'https://onlinepestestimates.herokuapp.com/auth/login-google'
                 } else {
                     window.location.href = 'http://localhost:4000/auth/login-google'
                 }
