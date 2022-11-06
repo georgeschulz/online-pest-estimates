9b49a6c161f08f511786a8ce340c8dcd02c6494c
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-11-05 20:09:14 -0400

    Test image webpack fix

--- widget/src/components/layout/banner.js
+++ widget/src/components/layout/banner.js
@@ -3,7 +3,7 @@ import defaultBanner from '../../assets/demoBanner.JPG';
 function Banner({ img = defaultBanner }) {
     return (
         <div className="pricing-widget-banner">
-            <img src={defaultBanner} className="pest-widget-banner" />
+            <img src={require('../../assets/demoBanner.JPG')} className="pest-widget-banner" />
         </div>
     );
 }
